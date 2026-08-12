d135d64ce23d94222c5b502fb26776fbdc5b9089
Alwan Wildan <alwan@example.com>
2026-08-12 07:47:56 +0700

    perf: disable pitch, rotation, and tile fading duration on Mapbox container for extreme mobile optimization

--- src/App.tsx
+++ src/App.tsx
@@ -447,6 +447,9 @@ export default function App() {
         maxZoom={18}
         reuseMaps
         antialias={false}
+        pitchWithRotate={false}
+        dragRotate={false}
+        fadeDuration={0}
         onClick={(e: any) => {
           if (navigationRoute) {
             // Lock map clicks during active navigation mode
